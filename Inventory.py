48f399d4fc6e0dfae3008c7b856f6631d5a21456
Anton Yefremov <anthyef@gmail.com>
2019-12-12 18:20:56 +0200

    draft implementation

--- Inventory.py
+++ Inventory.py
@@ -1,25 +1,39 @@
+#!/usr/bin/env python3
+import json
+from statistics import median
+
+from itertools import chain
+
 class Inventory(object):
     """
     Inventory Management system 
     """
-    def get_categories_for_stroe(self, store):
+
+    def __init__(self):
+        with open("inventory.txt") as fp:
+            self.items = json.load(fp)
+    
+    def get_categories_for_store(self, store):
         """
         Given a store id you should return all the categories ids in the inventory.
         :param store: store id
         :return: all the categories ids in the inventory
         """
-        pass
-    def get_item_inventory(self, item):
+        return list(filter(lambda item: item["store"] == store, self.items))
+    def get_item_inventory(self, item_name):
         """
         Given items name return all the items across all stores.
-        :param item: item name
+        :param item_name: item name
         :return: all the items across all stores
         """
-        pass
+        return list(filter(lambda item: item["item_name"] == item_name, self.items))
     def get_median_for_category(self, category):
         """
         Given category id return the median of the prices for all items in the category.
         :param category: category name
         :return: the median of the prices for all items in the category
         """
-        pass
+        items_in_category = filter(lambda item: item["category"] == category, self.items)
+
+        all_prices = chain.from_iterable([item["price"]] * item["items"] for item in items_in_category)
+        return median(all_prices)
