3ad87853c3c77c2e9c987767f4a6cf2064ecc16a
Anton Yefremov <anthyef@gmail.com>
2019-12-12 17:56:30 +0200

    Add scaffold files

---
+++ Inventory.py
@@ -0,0 +1,25 @@
+class Inventory(object):
+    """
+    Inventory Management system 
+    """
+    def get_categories_for_stroe(self, store):
+        """
+        Given a store id you should return all the categories ids in the inventory.
+        :param store: store id
+        :return: all the categories ids in the inventory
+        """
+        pass
+    def get_item_inventory(self, item):
+        """
+        Given items name return all the items across all stores.
+        :param item: item name
+        :return: all the items across all stores
+        """
+        pass
+    def get_median_for_category(self, category):
+        """
+        Given category id return the median of the prices for all items in the category.
+        :param category: category name
+        :return: the median of the prices for all items in the category
+        """
+        pass
