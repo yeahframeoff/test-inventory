e9dcf72dae36248e6de7dd5b74f6102b816e5efb
Anton Yefremov <anthyef@gmail.com>
2019-12-12 18:34:22 +0200

    refactor

--- Inventory.py
+++ Inventory.py
@@ -2,7 +2,7 @@
 import json
 from statistics import median
 
-from itertools import chain
+from itertools import chain, repeat
 
 class Inventory(object):
     """
@@ -12,6 +12,9 @@ class Inventory(object):
     def __init__(self):
         with open("inventory.txt") as fp:
             self.items = json.load(fp)
+
+    def _filter_items_by(self, predicate):
+        return list(filter(predicate, self.items))
     
     def get_categories_for_store(self, store):
         """
@@ -19,21 +22,35 @@ class Inventory(object):
         :param store: store id
         :return: all the categories ids in the inventory
         """
-        return list(filter(lambda item: item["store"] == store, self.items))
+        pred = lambda item: item["store"] == store
+        return self._filter_items_by(pred)
+
     def get_item_inventory(self, item_name):
         """
         Given items name return all the items across all stores.
         :param item_name: item name
         :return: all the items across all stores
         """
-        return list(filter(lambda item: item["item_name"] == item_name, self.items))
+        pred = lambda item: item["item_name"] == item_name
+        return self._filter_items_by(pred)
+
     def get_median_for_category(self, category):
         """
         Given category id return the median of the prices for all items in the category.
         :param category: category name
         :return: the median of the prices for all items in the category
         """
-        items_in_category = filter(lambda item: item["category"] == category, self.items)
+        pred = lambda item: item["category"] == category
+        items_in_category = self._filter_items_by(pred)
 
-        all_prices = chain.from_iterable([item["price"]] * item["items"] for item in items_in_category)
+        prices_and_counts = (
+            (item["price"], item["items"]) 
+            for item in items_in_category
+        )
+
+        all_prices = chain.from_iterable(
+            repeat(price, count)
+            for price, count in prices_and_counts
+        )
+        
         return median(all_prices)
