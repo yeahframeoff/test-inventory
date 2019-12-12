be33779ea2258c3cf3100626f6983d2abf5e05c2
Anton Yefremov <anthyef@gmail.com>
2019-12-12 19:19:06 +0200

    most of the tests

--- test_inventory.py
+++ test_inventory.py
@@ -31,3 +31,62 @@ class TestInventory:
     
     def test_get_categories_for_store_in_empty_inventory(self, empty_inventory):
         assert empty_inventory.get_categories_for_store(1) == []
+    
+    def test_get_item_inventory(self, inventory):
+        assert inventory.get_item_inventory("pc") == [
+            {
+                "store": 1,
+                "category": 1,
+                "item_name": "pc",
+                "items":1,
+                "price": 10
+            },
+            {
+                "store": 2,
+                "category": 1,
+                "item_name": "pc",
+                "items":3,
+                "price": 3
+            },
+        ]
+
+    def test_get_item_inventory(self, empty_inventory):
+        assert empty_inventory.get_item_inventory("pc") == []
+    
+    def test_get_median_for_category_basic(self):
+        items = [
+            {
+                "store": 1,
+                "category": 1,
+                "item_name": "The Item",
+                "items": 4,
+                "price": 200
+            },
+        ]
+
+        inventory = Inventory(items)
+
+        assert inventory.get_median_for_category(1) == 200
+    
+    def test_get_median_for_category_intermediate(self):
+        items = [
+            {
+                "store": 1,
+                "category": 1,
+                "item_name": "The Item",
+                "items": 4,
+                "price": 200
+            },
+            {
+                "store": 1,
+                "category": 1,
+                "item_name": "The Item Two",
+                "items": 4,
+                "price": 300
+            },
+        ]
+
+        inventory = Inventory(items)
+
+        assert inventory.get_median_for_category(1) == 250
+    
